8b727f61a814b2b8c7facaddf3993de071650961
unknown <abdullahsunasara6@gmail.com>
2024-09-22 12:13:56 +0530

    Blueprint

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,5 @@
-import os
 from dotenv import load_dotenv
-import base64
+# import base64
 from flask import Flask, request   
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -10,28 +9,43 @@ from email.mime.text import MIMEText
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from blog.config import Config
 
 
-app = Flask(__name__)
+
 
 load_dotenv()
+
 # Define the OAuth 2.0 scopes
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 CLIENT_SECRETS_FILE = "client_secret.json"
 
-# Configure Flask App
-app.config['SECRET_KEY'] = '6a2dd7adefc8975c1afffea5240a1fb7'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Blog.db'
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('abdullahcodes123@gmail.com')
-app.config['MAIL_PASSWORD'] = os.environ.get('Abd@#123')
-mail = Mail(app)
-from blog import routes
+
+mail = Mail()
+
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from blog.users.routes import users
+    from blog.posts.routes import posts
+    from blog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)  
+
+    return app  
